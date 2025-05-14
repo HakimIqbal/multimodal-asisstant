b3bae63ce6315257ad14afea2d1081efcd131e04
HakimIqbal <muh.iqbal.hakim@gmail.com>
2025-05-14 12:13:27 +0700

    update

--- test_pdf2image.py
+++
@@ -1,6 +0,0 @@
-from pdf2image import convert_from_path
-try:
-    images = convert_from_path("fileujicoba-pdf-png-docx-jpeg-jpg/dasarpemrogramanpython.pdf")
-    print(f"Berhasil mengonversi {len(images)} halaman.")
-except Exception as e:
-    print(f"Error: {str(e)}")
